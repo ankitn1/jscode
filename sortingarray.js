6bf4db7c15b71bcac5814671f1e7f502b15a5dcb
ankitn1 <ankitnagrale2912@gmail.com>
2023-10-26 01:24:50 +0530

    first commit

--- sortingarray.js
+++ sortingarray.js
@@ -1,9 +1,16 @@
-const arr1 = [10,9,15,14,22.5,18.6]; 
-
-function arrSort(arr) { 
-	arr.sort((a,b)=>a-b); 
-	arr.reverse(); 
-	return arr; 
-} 
-
-console.log(arrSort(arr1)); 
+function customDescendingSort(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+	  for (let j = i + 1; j < arr.length; j++) {
+		if (arr[i] < arr[j]) {
+		  const temp = arr[i];
+		  arr[i] = arr[j];
+		  arr[j] = temp;
+		}
+	  }
+	}
+  }
+  
+  const numbers = [10,8,25,15,22,35];
+  customDescendingSort(numbers);
+  console.log(numbers);
+  
